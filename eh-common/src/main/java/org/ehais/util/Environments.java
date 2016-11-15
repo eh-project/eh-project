a1bcb014c0580cafb77384371170f32566e3fe68
lgj628 <lgj628@126.com>
2016-11-15 23:13:15 +0800

    team
    Signed-off-by: Yang <zjy799281327@qq.com>

---
+++ eh-common/src/main/java/org/ehais/util/Environments.java
@@ -0,0 +1,22 @@
+package org.ehais.util;
+
+/**
+ * 环境变量的配置
+ * @author lgj628
+ *
+ */
+public class Environments {
+	
+	private Integer storeId;
+
+	public Integer getStoreId() {
+		return storeId;
+	}
+
+	public void setStoreId(Integer storeId) {
+		this.storeId = storeId;
+	}
+
+	
+
+}
