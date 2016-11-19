975bc606fab89d9068d23d5b3cf8babe796a6fd6
lgj628 <lgj628@126.com>
2016-11-19 11:59:25 +0800

    init gxrc

---
+++ eHR-Lucene/src/main/java/com/ehais/hrlucene/service/GxrcService.java
@@ -0,0 +1,6 @@
+package com.ehais.hrlucene.service;
+
+public interface GxrcService {
+
+	
+}
---
+++ eHR-Lucene/src/main/java/com/ehais/hrlucene/service/impl/GxrcServiceImpl.java
@@ -0,0 +1,10 @@
+package com.ehais.hrlucene.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.ehais.hrlucene.service.GxrcService;
+
+@Service("gxrcService")
+public class GxrcServiceImpl implements GxrcService {
+
+}
