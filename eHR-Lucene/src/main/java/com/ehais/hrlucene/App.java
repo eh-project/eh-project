854a134ac07df09401ae05b4494dd7ca3eed3a87
lgj628 <lgj628@126.com>
2016-11-23 09:39:47 +0800

    pom - xml-apis

---
+++ eHR-Lucene/src/main/java/com/ehais/hrlucene/App.java
@@ -0,0 +1,29 @@
+package com.ehais.hrlucene;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.ehais.hrlucene.service.HnrcService;
+
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+        System.out.println( "Hello Hn!" );
+        
+//        ApplicationContext context = new FileSystemXmlApplicationContext("spring/appContext.xml"); 
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring/appContext.xml"); 
+        
+//        GxrcService gxrcService = (GxrcService)context.getBean("gxrcService");
+//        gxrcService.loadGxrc();
+        
+        HnrcService hnrcService = (HnrcService)context.getBean("hnrcService");
+        hnrcService.loadHnrc();
+        
+//        SpringTaskService springTaskService = (SpringTaskService)context.getBean("springTaskService");
+//        
+//        springTaskService.myTask();
+        
+    }
+
+}
