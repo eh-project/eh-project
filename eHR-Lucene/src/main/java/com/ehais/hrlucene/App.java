82ebdf98f90fdc2004cb3c5348a586dea20303b0
lgj628 <lgj628@126.com>
2016-11-29 22:56:14 +0800

    school

--- eHR-Lucene/src/main/java/com/ehais/hrlucene/App.java
+++ eHR-Lucene/src/main/java/com/ehais/hrlucene/App.java
@@ -1,12 +1,9 @@
 package com.ehais.hrlucene;
 
 import org.springframework.context.ApplicationContext;
-
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.ehais.hrlucene.service.GxrcService;
-import com.ehais.hrlucene.service.ScrcService;
-import com.ehais.hrlucene.service.FjrcService;
+import com.ehais.hrlucene.service.YnrcService;
 
 /**
  * Hello world!
@@ -18,21 +15,30 @@ public class App
     {
         System.out.println( "Hello World!" );
         
-//        ApplicationContext context = new FileSystemXmlApplicationContext("spring/appContext.xml"); 
+//        ApplicationContext context = new FileSystemXmlApplicationContext(new String[]    {"classpath*:appContext.xml","classpath*:appContext-jdbc.xml"}); 
         ApplicationContext context = new ClassPathXmlApplicationContext("spring/appContext.xml"); 
+//        ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:spring/appContext.xml"); 
+//        ApplicationContext context = new ClassPathXmlApplicationContext(new String[]    {"appContext.xml","appContext-jdbc.xml"}); 
         
 //        GxrcService gxrcService = (GxrcService)context.getBean("gxrcService");
 //        gxrcService.loadGxrc();
         
-        //FjrcService fjrcService = (FjrcService)context.getBean("FjrcService");
-        //fjrcService.LoadFjrc();
-        
-        ScrcService scrcService=(ScrcService)context.getBean("ScrcService");
-        scrcService.Loadrc();
+//        FjrcService fjrcService = (FjrcService)context.getBean("FjrcService");
+//        fjrcService.LoadFjrc();
         
 //        SpringTaskService springTaskService = (SpringTaskService)context.getBean("springTaskService");
 //        
 //        springTaskService.myTask();
+        
+//        HaiNanrcService service = (HaiNanrcService)context.getBean("hainanrcService");
+//        service.loadHaiNanrc();
+        
+//        HnrcService service = (HnrcService)context.getBean("hnrcService");
+//        service.loadHnrc();
+        
+        YnrcService serivce = (YnrcService)context.getBean("ynrcService");
+        serivce.loadGxrc();
+        
         
     }
 }
