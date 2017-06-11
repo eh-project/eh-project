79b1f0c37ed48533cfaa613958550d95ba87779a
oldbiwang <1063049578wrp@gmail.com>
2017-06-11 16:11:15 +0800

    'theclutcherController'

--- FigoShop/src/main/java/com/ehais/figoarticle/controller/SixpmController.java
+++ FigoShop/src/main/java/com/ehais/figoarticle/controller/SixpmController.java
@@ -12,6 +12,8 @@ import org.ehais.util.Bean2Utils;
 import org.ehais.util.EHttpClientUtil;
 import org.ehais.util.PythonUtil;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -33,14 +35,13 @@ import net.sf.json.JSONObject;
 @Controller
 @RequestMapping("/6pm")
 public class SixpmController extends FigoCommonController{
-	private static String url = "www.6pm.com";
+	private static String url = "http://www.6pm.com/";
 	private int websiteId = 18;
 
 
 	@ResponseBody
 	@RequestMapping("/brand")
 	public String brand(ModelMap modelMap, HttpServletRequest request, HttpServletResponse response){
-		
 		return "";
 	}
 	
@@ -49,9 +50,7 @@ public class SixpmController extends FigoCommonController{
 	public String category(ModelMap modelMap, HttpServletRequest request, HttpServletResponse response){
 		
 		try {
-			
 			this.category(request, url);
-
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -64,13 +63,24 @@ public class SixpmController extends FigoCommonController{
 	private void category(HttpServletRequest request,String categoryUrl){
 		String result = "";
 		try {
-			result = PythonUtil.python(request.getRealPath("/getAjaxWeb.py"), categoryUrl);
-//			result = PythonUtil.python("D:/workspace_jee/figoarticle/src/main/webapp/getAjaxWeb.py", categoryUrl);
-//			result = FSO.ReadFileName("E:/temp/IFCHIC.htm");
-			Document doc = Jsoup.parse(result);
+			//result = PythonUtil.python(request.getRealPath("/getAjaxWeb.py"), categoryUrl);
+			//Document doc = Jsoup.parse(result);
+			
 			List<HaiCategory> list = new ArrayList<HaiCategory>();
 			
-			// TODO 
+			// TODO 		
+			result = GetPostTest.sendGet(categoryUrl, null);
+			Document doc = Jsoup.parse(result);
+			Element header = doc.getElementsByClass("header").first();
+			Element div = header.select(".header-nav.refresh-container").first();
+			Element ul = div.getElementsByTag("ul").first();
+			Elements menuLis = ul.select(">li");
+			System.out.println(menuLis);
+			String parentHref = "";
+			for(Element li : menuLis) {
+				Element a = li.select(">a").first();
+				
+			}
 			
 			
 			
@@ -109,8 +119,6 @@ public class SixpmController extends FigoCommonController{
 				System.out.println(haiCategory.getCategoryUrl());
 				this.goodsUrl(request, haiCategory.getCategoryUrl(), haiCategory.getCatId());
 			}
-			
-
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -124,31 +132,16 @@ public class SixpmController extends FigoCommonController{
 		String result = "";
 		try{
 			result = PythonUtil.python(request.getRealPath("/getAjaxWeb.py"), goodsurl);
-//			result = PythonUtil.python("D:/workspace_jee/figoarticle/src/main/webapp/getAjaxWeb.py", categoryUrl);
-//			result = FSO.ReadFileName("E:/temp/IFCHIC.htm");
 			Document doc = Jsoup.parse(result);
 			List<String> list = new ArrayList<String>();
 
 			// TODO
-			
-			
 			JSONArray arr = JSONArray.fromObject(list);
 			Map<String, String> paramsMap = new HashMap<String,String>();
 			paramsMap.put("catId", catId.toString());
 			paramsMap.put("json", arr.toString());
-//			String api = request.getScheme()+"://"+ request.getServerName()+":"+request.getServerPort()+"/api/url";
 			String api = "http://localhost:8087/api/url";
 			String apiresult = EHttpClientUtil.httpPost(api, paramsMap);
-			
-			//获取下一页
-			/**			 
-			Element page_chooser = page.getElementById("page-chooser");
-			Element next = page_chooser.getElementsByClass("next").first();
-			if(next != null){
-				String href_a = next.attr("href");
-				this.goodsUrl(request, href_a, catId);
-			}
-			**/
 		}catch(Exception e){
 			e.printStackTrace();
 		}
@@ -187,17 +180,10 @@ public class SixpmController extends FigoCommonController{
 		HaiGoodsWithBLOBs goods = new HaiGoodsWithBLOBs();
 		List<HaiGoodsGallery> goodsGalleryList = new ArrayList<HaiGoodsGallery>();
 		List<HaiGoodsAttr> goodsAttrList = new ArrayList<HaiGoodsAttr>();
-		HaiGoodsAttr goodsAttr = new HaiGoodsAttr();
 		goods.setGoodsUrl(goodsurl);
 		goods.setCatId(catId);
 		goods.setWebsiteId(websiteId);
 		try{
-
-			
-			
-			
-			
-			
 			Bean2Utils.printEntity(goods);
 			
 			entity.setGoods(goods);
@@ -207,11 +193,6 @@ public class SixpmController extends FigoCommonController{
 			System.out.println(jsonObject.toString());
 			Map<String, String> paramsMap = new HashMap<String,String>();
 			paramsMap.put("json", jsonObject.toString());
-//			String api = request.getScheme()+"://"+ request.getServerName()+":"+request.getServerPort()+"/api/goods";
-//			String api = "http://localhost:8087/api/goods";
-//			String apiresult = EHttpClientUtil.httpPost(api, paramsMap);
-			
-			
 			
 		}catch(Exception e){
 			e.printStackTrace();
@@ -223,11 +204,5 @@ public class SixpmController extends FigoCommonController{
 	public static void main(String[] args) throws Exception {
 		String goodsurl = "https://www.net-a-porter.com/cn/zh/d/Shop/Lingerie/All?cm_sp=topnav-_-clothing-_-lingerie";
 		DemoController ac = new DemoController();
-//		ac.goodsModel(url,1);
-//		ac.goodsUrl(null, goodsurl, 1);
 	}
-	
-	
-	
-	
 }
